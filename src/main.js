06ae26fbdbd9b74f5267cc565e811e2cd15eae0a
wuchrchi <sq12348338@gmail.com>
2025-07-11 13:30:42 +0800

    忘了換ROUTER

--- src/main.js
+++ src/main.js
@@ -46,14 +46,14 @@ const routes = [
 
 ];
 
-// const router = createRouter({
-//    history: createWebHistory("/muulin-web/"),
-//   routes,
-// });
 const router = createRouter({
-   history: createWebHistory("/"),
+   history: createWebHistory("/muulin-web/"),
   routes,
 });
+// const router = createRouter({
+//    history: createWebHistory("/"),
+//   routes,
+// });
 
 loadFonts();
 
